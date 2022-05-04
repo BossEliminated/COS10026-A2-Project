57816da297c9868cb7c7961ec4b831acf901d07d
ChristopherTTT <103995646@student.swin.edu.au>
2022-05-04 15:44:30 +1000

    boop

---
+++ markquiz.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="Authors" content="Nicholas, Syed, Christopher, Yash">
+	<meta name="Keywords" content="Verifing the server">
+	<meta name="Description" content="Marking the quiz on the server side allowing to sending to the database">
+</head>
+<body>
+<?php 
+
+
+
+
+?>
+
+</body>
+</html>
