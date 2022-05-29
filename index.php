349d2a5ebe5b42d4b8ca376d578e0badd6856e53
BossEliminated <spongebob.squarepants662@gmail.com>
2022-05-30 01:18:59 +1000

    Added video link 2 and favicon

--- index.php
+++ index.php
@@ -11,8 +11,14 @@
       <div class="index-container">
         <p>Apache is a free and open-source software that allows users to deploy their websites on the internet. It is one of the oldest and most reliable web server software maintained by the Apache Software Foundation, with the first version released in 1995.<br></p>
         <div class="index-video">
-          <a class="show-tooltip" href="https://www.youtube.com/watch?v=vYuctGw5dBc" target="_blank">Video Tour</a>
-          <p class="tooltip tooltip-large">Apache Project Video</p>
+          <div>
+            <a class="show-tooltip" href="https://www.youtube.com/watch?v=vYuctGw5dBc" target="_blank">Part A Video Tour</a>
+            <p class="tooltip">Project Video Part A</p>
+          </div>
+          <div>
+            <a class="show-tooltip" href="https://www.youtube.com/watch?v=IRkolhiP04k" target="_blank">Part B Video Tour</a>
+            <p class="tooltip">Project Video Part B</p>
+          </div>
         </div>
       </div>
       <div class="index-flex-container">
